1800df5de3084b414e0f179a857f16eb79f746bc
devRealpe <johan.devrealpe@gmail.com>
2026-02-18 16:55:48 -0500

    cambios sin terminar

--- apps/mi-app/src/app/pages/profile/profile.components.ts
+++ apps/mi-app/src/app/pages/profile/profile.components.ts
@@ -4,11 +4,27 @@ import { TagModule } from 'primeng/tag';
 import { ChipModule } from 'primeng/chip';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { FormsModule } from '@angular/forms';
+
+  interface City{
+    cname: string;
+  }
+
+  interface State {
+    name: string;
+    cities: City[];
+  }
+
+  interface Country {
+  name: string;
+  states: State[];
+}
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CardModule, TagModule, ChipModule, AvatarModule, ButtonModule],
+  imports: [CardModule, TagModule, ChipModule, AvatarModule, ButtonModule, FormsModule, CascadeSelectModule],
   templateUrl: './profile.html',
 })
 export class ProfileComponent {
@@ -24,4 +40,30 @@ export class ProfileComponent {
     const htmlElement = document.documentElement;
     htmlElement.classList.toggle('dark');
 }
+
+  selectedCity: City | null = null;
+  
+  countries: Country[] = [
+    {
+      name: 'USA',  
+      states: [
+        {
+          name: 'California',
+          cities: [
+            { cname: 'Los Angeles' },
+            { cname: 'San Francisco' },
+            { cname: 'San Diego' }
+          ]
+        },
+        {
+          name: 'New York',
+          cities: [
+            { cname: 'New York City' },
+            { cname: 'Buffalo' },
+            { cname: 'Rochester' }
+          ]
+        }
+      ],
+    },
+  ];
 }
